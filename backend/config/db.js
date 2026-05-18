4a4035809fbfc9428faf99c58ea028e2845ce32b
soufyane <soufyanesahi20004@gmail.com>
2026-05-19 00:07:01 +0100

    Use database port and SSL for Aiven MySQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,12 +4,16 @@ const mysql = require("mysql2/promise");
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = db;
