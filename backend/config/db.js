43d114fa117f8c96e3d870099cc1bdc5e336e20f
soufyane <soufyanesahi20004@gmail.com>
2026-05-19 22:44:15 +0100

    Fix

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,15 @@
-const path = require("path");
-
-require("dotenv").config({
-  path: path.join(__dirname, "..", ".env"),
-  quiet: true,
-});
-
-const mysql = require("mysql2/promise");
-
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = db;
